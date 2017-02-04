0a2fb3624ee3a3dd47e758c6dbd37a08002b4f24
izelenev <the_lenev@mail.ru>
2017-02-04 12:40:34 +0300

    Add book in database with validation

--- protected/controllers/BookController.php
+++ protected/controllers/BookController.php
@@ -4,6 +4,10 @@ class BookController extends Controller
 {
     public function actionIndex()
 	{
-		echo 1;
+		$model = new Book;
+        $model -> title = 'Война и мир';
+        $model -> description = 'Л. Толстой';
+        $model -> save();
+        echo 2;
 	}
 }
