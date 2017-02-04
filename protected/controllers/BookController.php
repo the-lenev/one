e00629686c7891af4cd33839e19f80cff1b89fe7
izelenev <the_lenev@mail.ru>
2017-02-04 12:44:54 +0300

    Add book in database without validation

--- protected/controllers/BookController.php
+++ protected/controllers/BookController.php
@@ -4,6 +4,10 @@ class BookController extends Controller
 {
     public function actionIndex()
 	{
-		echo 1;
+        $model = new Book;
+        $model -> title = 'Война и мир';
+        $model -> title = 'Л. Толстой';
+        $model -> save(false);
+		echo 'master';
 	}
 }
